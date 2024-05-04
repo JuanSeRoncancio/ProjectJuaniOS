99126ada475f9da8ca660a432aaf1dcd2b299587
juan.roncancio <sbm029@gmail.com>
2024-05-03 21:58:22 -0500

    Initial Commit

---
+++ ProjectJuan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectJuan
+//
+//  Created by juan.roncancio on 3/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectJuanUITests/ProjectJuanUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProjectJuanUITestsLaunchTests.swift
+//  ProjectJuanUITests
+//
+//  Created by juan.roncancio on 3/05/24.
+//
+
+import XCTest
+
+final class ProjectJuanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
